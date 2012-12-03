74ff1568c4773b2570316773b563d06a194c24d0
Benedikt Ahrens <benedikt.ahrens@gmx.net>
2012-12-03 14:15:53 -0500

    renamed aux file

--- HLevel_n_is_of_hlevel_Sn.v
+++ HLevel_n_is_of_hlevel_Sn.v
@@ -8,8 +8,6 @@ Require Import hProp.
 Require Import funextfun.
 
 
-
-
 Notation "a == b" := (paths a b) (at level 70, no associativity).
 Notation "! p " := (pathsinv0 p) (at level 50).
 Notation "p @ q" := (pathscomp0 p q) (at level 60, right associativity).
